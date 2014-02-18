ae97184eccd58ca3fbd8bce70d4fb14dbea02d77
Yong Yun Jing <yongyunjing@hack-a-mac.gateway.2wire.net>
2014-02-18 21:40:41 +0800

    First push

---
+++ RSSEntry.h
@@ -0,0 +1,13 @@
+//
+//  RSSEntry.h
+//  RSS206
+//
+//  Created by Yong Yun Jing on 18/2/14.
+//  Copyright (c) 2014 Yong Yun Jing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSSEntry : NSObject
+
+@end
---
+++ RSSEntry.m
@@ -0,0 +1,13 @@
+//
+//  RSSEntry.m
+//  RSS206
+//
+//  Created by Yong Yun Jing on 18/2/14.
+//  Copyright (c) 2014 Yong Yun Jing. All rights reserved.
+//
+
+#import "RSSEntry.h"
+
+@implementation RSSEntry
+
+@end
